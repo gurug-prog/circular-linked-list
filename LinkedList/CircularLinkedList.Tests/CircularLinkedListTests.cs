be7bd9eb884ffc07be3cdea1c1f6ae9778e77f6b
gurug-prog <ivanmysterys@gmail.com>
2022-06-22 23:12:34 +0300

    feat: add deleteAll tests

--- LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
+++ LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
@@ -69,4 +69,33 @@ public class CircularLinkedListTests
 
         Assert.Equal(expectedEl, actualEl);
     }
+
+    [Theory]
+    [InlineData('f', 2, new char[]{'b', 'u', 'f', 'f'})]
+    [InlineData('x', 2, new char[]{'x', 'y', 'z'})]
+    [InlineData('e', 6, new char[]{'H', 'i', ',', 't', 'h', 'e', 'r', 'e'})]
+    [InlineData('z', 0, new char[]{'z'})]
+    public void TestDeleteAll_ResultAllMatchingElementsDeleted
+        (char element, int lenExpected, char[] testValues)
+    {
+        FillTestValuesIntoList(testValues);
+
+        _list.DeleteAll(element);
+
+        Assert.Equal(lenExpected, _list.Length());
+    }
+
+    [Theory]
+    [InlineData('l', new char[]{'x', 'y', 'z'})]
+    [InlineData('d', new char[]{'H', 'i', ',', 't', 'h', 'e', 'r', 'e'})]
+    [InlineData('a', new char[]{'z'})]
+    public void TestDeleteAll_ResultNothingChanged
+    (char element, char[] testValues)
+    {
+        FillTestValuesIntoList(testValues);
+
+        _list.DeleteAll(element);
+
+        Assert.Equal(testValues.Length, _list.Length());
+    }
 }
