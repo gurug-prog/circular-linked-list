6bcac93b3503aa6eb1923a42408a5170e0c48cc2
gurug-prog <ivanmysterys@gmail.com>
2022-06-22 21:27:06 +0300

    chore: add list dependencies to test project

--- LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
+++ LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
@@ -4,9 +4,18 @@ namespace LinkedList.CircularLinkedList.Tests;
 
 public class CircularLinkedListTests
 {
+    private readonly ICircularLinkedList _list;
+
+    public CircularLinkedListTests()
+    {
+        _list = new CircularLinkedList();
+    }
+
     [Fact]
     public void Test1()
     {
+        _list.Append('a');
 
+        // Assert
     }
 }
