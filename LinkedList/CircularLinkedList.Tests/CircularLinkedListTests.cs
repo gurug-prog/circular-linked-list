e7ada7f951e328a61825a4b47e422ee99b69ddbf
gurug-prog <ivanmysterys@gmail.com>
2022-06-22 22:42:08 +0300

    feat: add basic method tests

--- LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
+++ LinkedList/CircularLinkedList.Tests/CircularLinkedListTests.cs
@@ -11,11 +11,62 @@ public class CircularLinkedListTests
         _list = new CircularLinkedList();
     }
 
-    [Fact]
-    public void Test1()
+    private void FillTestValuesIntoList(char[] testValues)
     {
-        _list.Append('a');
+        foreach (var el in testValues)
+        {
+            _list.Append(el);
+        }
+    }
 
-        // Assert
+    [Fact]
+    public void TestAppend_ResultElementWasAppended()
+    {
+        var element = 'a';
+        var lenExpected = 1;
+
+        _list.Append(element);
+
+        Assert.Equal(lenExpected, _list.Length());
+    }
+
+    [Theory]
+    [InlineData(0, new char[]{ 'a', 'b', 'c', 'd' })]
+    [InlineData(2, new char[]{ 'b', 'o', 'o', 'm', '!' })]
+    public void TestDelete_ResultElementWasDeleted(int index, char[] testValues)
+    {
+        FillTestValuesIntoList(testValues);
+        var lenExpected = testValues.Length - 1;
+
+        _list.Delete(index);
+
+        Assert.Equal(lenExpected, _list.Length());
+    }
+
+    [Theory]
+    [InlineData(new char[]{ 'c', 'l', 'e', 'a', 'r' })]
+    public void TestClear_ResultListWasCleared(char[] testValues)
+    {
+        FillTestValuesIntoList(testValues);
+        var lenExpected = 0;
+
+        _list.Clear();
+
+        Assert.Equal(lenExpected, _list.Length());
+    }
+
+    [Theory]
+    [InlineData('f', 3, new char[]{'b', 'u', 'f', 'f'})]
+    [InlineData('y', 1, new char[]{'x', 'y', 'z'})]
+    [InlineData('h', 4, new char[]{'H', 'i', ',', 't', 'h', 'e', 'r', 'e'})]
+    [InlineData('z', 0, new char[]{'z'})]
+    public void TestGet_ResultMathesElement
+        (char expectedEl, int index, char[] testValues)
+    {
+        FillTestValuesIntoList(testValues);
+
+        var actualEl = _list.Get(index);
+
+        Assert.Equal(expectedEl, actualEl);
     }
 }
